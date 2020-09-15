46e4d755e0b04de37bc9780056af05e432b5a143
Ignasi Camacho Redó <34693181+IgnasiCR@users.noreply.github.com>
2020-09-15 17:11:11 +0200

    Añadidas cabeceras del bot.

--- src/telegrambot/moviebot.py
+++ src/telegrambot/moviebot.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
 
 import pyrogram as pg
+import os
 
+def enviarMensaje(idChat, texto):
+  pass
+
+def enviarFoto(idChat, foto):
+  pass
+
+def respuestaInline(idInline, results):
+  pass
