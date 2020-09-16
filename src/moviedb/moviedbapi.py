79d6ef96c6300a322e770f0023cbae1582978f04
ManuelJNunez <manueljesusnunezruiz@gmail.com>
2020-09-16 17:27:07 +0200

    Añadida nueva función al módulo moviedbapi #24 e implementado todo el código del módulo

--- src/moviedb/moviedbapi.py
+++ src/moviedb/moviedbapi.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python
-
+import os
 import requests as rq
+from dotenv import load_dotenv
+from pathlib import Path
 
-def busqueda(query):
-    pass
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
 
-def busca_relacionadas(movie_id):
-    pass
+TMDb_KEY = os.getenv('TMDb_KEY')
+language = os.getenv('language')
+
+def realiza_peticion(url: str):
+    res = rq.get(url)
+    res = res.json()['results'] if res.json()['results'] else None
+
+    return res
+
+
+def busqueda(query: str):
+    return realiza_peticion(f'https://api.themoviedb.org/3/search/movie?api_key={TMDb_KEY}&query={query}&language={language}')
+
+
+def busca_relacionadas(movie_id: int):
+    return realiza_peticion(f'https://api.themoviedb.org/3/movie/{movie_id}/recommendations?api_key={TMDb_KEY}&language={language}')
 
