69d534f7281b1e99ec97659cf7da654e9a6db97c
Manuel Jesús Núñez Ruiz <34189938+ManuelJNunez@users.noreply.github.com>
2020-09-18 19:23:41 +0200

    Refinado el código un poco más. Close #24 (#28)
    * Arreglado fichero mal escrito
    
    * Closes #6
    
    * Arreglado fallo en qa.json Close #22
    
    * Añadida nueva función al módulo moviedbapi #24 e implementado todo el código del módulo
    
    * Refinado un poco mas el modulo moviebot #24
    
    * Refinado el modulo para crear las tablas de la BD (bdinitializer) #24
    
    * Actualiado qa.json con el nombre del fichero
    
    * Refinado el modulo inicializador de las tablas de la BD (bdinitializer) #24
    
    * Terminado refinamiento. Close #24

--- src/database/bd.py
+++
@@ -1,3 +0,0 @@
-import os
-from playhouse.postgres_ext import PostgresqlExtDatabase
-
---
+++ src/database/bdinitializer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import os
+import pathlib as pl
+from playhouse.postgres_ext import *
+from dotenv import load_dotenv
+
+env_path = pl.Path(__file__).parent / '.env'
+load_dotenv(dotenv_path=env_path)
+
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASS = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+
+psql_db = PostgresqlExtDatabase(DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+
+class UserInfo(Model):
+    user_id = IntegerField(primary_key=True)
+    favourite_films = ArrayField(IntegerField)
+
+    class Meta:
+        database = psql_db
+    
+if __name__ == '__main__':
+    psql_db.connect()
+    psql_db.create_tables([UserInfo])
+    psql_db.close()
